fdbb71b7e3fed51feb794bf6f09bb5e62dfd7960
Taysir96 <91126653+Taysir96@users.noreply.github.com>
2023-04-18 23:39:29 +0200

    home en menu

---
+++ script/index.js
@@ -0,0 +1,15 @@
+const slides = ['banner-1.jpg', 'home-2.jpg', 'home-4.jpg', 'home-6.jpg', 'home-5.jpg'];
+let currentSlide = 0;
+const slideshow = document.querySelector('.banner-menu');
+
+function showSlide(n) {
+    currentSlide = (n + slides.length) % slides.length;
+    slideshow.style.backgroundImage = `url(../images/${slides[currentSlide]})`;
+}
+
+showSlide(0); // Toon de eerste slide
+
+// Automatisch overschakelen naar de volgende slide na 3 seconden
+setInterval(() => {
+    showSlide(currentSlide + 1);
+}, 3000);
