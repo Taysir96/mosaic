9e1f646e1849243b10a3aac88cd474ef5a81f15d
Taysir96 <91126653+Taysir96@users.noreply.github.com>
2023-05-01 14:38:34 +0200

    test

--- script/index.js
+++ script/index.js
@@ -1,17 +1,32 @@
-const slides = ['banner-1.jpg', 'home-2.jpg', 'home4.jpg', 'home-6.jpg', 'home-5.jpg'];
-let currentSlide = 0;
-const slideshow = document.querySelector('.banner-menu');
+// const slides = ['banner-1.jpg', 'home-2.jpg', 'home4.jpg', 'home-6.jpg', 'home-5.jpg'];
+// let currentSlide = 0;
+// const slideshow = document.querySelector('.banner-menu');
 
-function showSlide(n) {
-    currentSlide = (n + slides.length) % slides.length;
-    slideshow.style.backgroundImage = `url(./images/${slides[currentSlide]})`;
+// function showSlide(n) {
+//     currentSlide = (n + slides.length) % slides.length;
+//     slideshow.style.backgroundImage = `url(./images/${slides[currentSlide]})`;
+// }
+
+// showSlide(0);
+
+// setInterval(() => {
+//     showSlide(currentSlide + 1);
+// }, 3000);
+
+var images = ["../images/menu-card.jpg", "../images/menu-card2.jpg"];
+var currentIndex = 0;
+var slideshow = document.querySelector('.banner-menu');
+
+function slideImage() {
+    slideshow.style.backgroundImage = 'url(' + images[currentIndex] + ')';
+    currentIndex++;
+    if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
 }
 
-showSlide(0);
+setInterval(slideImage, 3000);
 
-setInterval(() => {
-    showSlide(currentSlide + 1);
-}, 3000);
 document.body.onmousemove = function (e) {
     document.documentElement.style.setProperty(
         '--x', (
